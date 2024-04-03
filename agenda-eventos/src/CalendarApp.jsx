4c0a90ba395d0701d1a75dbcbccb35eafa743fd7
Elias Segura <eliasjsegura00@gmail.com>
2024-04-03 16:42:49 -0600

    manejo de las rutas

--- agenda-eventos/src/CalendarApp.jsx
+++ agenda-eventos/src/CalendarApp.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
 import { AppRouter } from "./router"
 
 function CalendarApp() {
   return (
-    <>
+    <BrowserRouter>
       <AppRouter />
-    </>
+    </BrowserRouter>
   )
 }
 
--- agenda-eventos/src/router/AppRouter.jsx
+++ agenda-eventos/src/router/AppRouter.jsx
@@ -1,7 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth' 
+import { CalendarPage } from '../calendar'
+
 export const AppRouter = () => {
+    const authStatus = 'not-authenticated' // esto seria temporal
+
     return (
-        <div>
-            Esta es la pagina de rutas
-        </div>
+        <Routes>
+            {
+                ( authStatus === 'not-authenticated')
+                ? <Route path='/auth/*' element={ <LoginPage/> }/>
+                : <Route path='*' element={ <CalendarPage/> } />
+            }
+
+            <Route path='*' element={ <Navigate to={'/auth/Login'} /> } />
+        </Routes>
     )
 }

8a186fe6cb3b8abad7e2fe74cc59a936d486771d
Elias Segura <eliasjsegura00@gmail.com>
2024-04-04 16:14:15 -0600

    changes on calendarpage

--- agenda-eventos/src/calendar/pages/CalendarPage.jsx
+++ agenda-eventos/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,31 @@
 import { NavBar } from '../'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+})
 
 export const CalendarPage = () => {
     return (
         <>
             <NavBar/>
+
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </>
     )
 }

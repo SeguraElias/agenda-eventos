47f67e76468171653697297e82b1aead0ac09fcf
Elias Segura <eliasjsegura00@gmail.com>
2024-04-04 16:16:18 -0600

    changes on calendarpage

--- agenda-eventos/src/calendar/pages/CalendarPage.jsx
+++ agenda-eventos/src/calendar/pages/CalendarPage.jsx
@@ -6,6 +6,10 @@ import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
 
+const locales = {
+    'en-US': enUS
+}
+
 const localizer = dateFnsLocalizer({
     format,
     parse,

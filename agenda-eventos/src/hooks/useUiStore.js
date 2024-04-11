27178e02b30ae90490dc3cd6ac916f16df07c8f4
Elias Segura <eliasjsegura00@gmail.com>
2024-04-11 16:02:08 -0600

    agregue funciones al useUiStore

--- agenda-eventos/src/hooks/useUiStore.js
+++ agenda-eventos/src/hooks/useUiStore.js
@@ -1,7 +1,16 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { onOpenDateModal } from '../store/ui/uiSlice'
 
 export const useUiStore = () => {
-    const { isDateModalOpen } = useSelector( state => state.ui )
+    const { 
+        isDateModalOpen 
+    } = useSelector( state => state.ui )
+
+    const dispatch = useDispatch()
+
+    const openDateModal = () => {
+        dispatch( onOpenDateModal() )
+    }
 
     return {
         // * Propiedades
